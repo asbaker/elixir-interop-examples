26ea2f343aa8c849eed8a6ff5dccce68b9c9b55f
Tony Baker <tony.baker@atomicobject.com>
2015-03-04 18:27:19 -0500

    Add arduino random serial text

---
+++ arduino_random_serial_text/src/sketch.ino
@@ -0,0 +1,21 @@
+#define ARRAY_SIZE 4
+
+static String data[ARRAY_SIZE] = {
+  "This is some data.",
+  "42",
+  "{watch_out: \"For JSON\"}",
+  "Woohoo data data data!!!"
+};
+
+int i;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  i = random(0,ARRAY_SIZE);
+
+  Serial.println(data[i]);
+  delay(2000);
+}
